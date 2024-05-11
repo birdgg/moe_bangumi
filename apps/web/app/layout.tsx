a54a96befcacda36b41c651fa6ed4eee92996d17
birdgg <birdeggegg@gmail.com>
2024-05-12 00:54:53 +0800

    feat: add dark mode

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,21 @@
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { fontSans } from "@/lib/fonts";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Moe Bangumi",
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({
@@ -15,12 +25,22 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="fixed w-full py-5 bg-blue-500"></div>
-        <div className="flex flex-row h-[200px] pt-[40px]">
-          <div className="h-full w-[30px] bg-pink-300"></div>
-          {children}
-        </div>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative flex min-h-screen flex-col bg-background">
+            {children}
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
