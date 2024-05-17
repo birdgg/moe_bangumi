4716a9cac277dacca1a5127b655534c1d1c5e63e
birdgg <birdeggegg@gmail.com>
2024-05-17 13:20:05 +0800

    feat: tsconfig, eslint

---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,25 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "eslint-config-turbo",
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    '.eslintrc.js'
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
