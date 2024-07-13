3fcfc853d972b7fe26c80fad301af09af1ef6696
birdgg <birdeggegg@gmail.com>
2024-07-13 16:04:40 +0800

    feat: add qbittorrent client

---
+++ apps/server/src/libs/qbittorrent/index.ts
@@ -0,0 +1,93 @@
+import axios, { AxiosInstance } from 'axios';
+import * as https from 'https';
+import { TorrentListOptions, TorrentOptions } from './types';
+import { isDev } from '@/utils/env';
+
+const API_PATH = {
+  login: 'auth/login',
+};
+
+export class Qbittorent {
+  #host: string;
+  #username: string;
+  #password: string;
+  #sid = '';
+  #axios: AxiosInstance;
+  isConnected = false;
+
+  constructor(host: string, username: string, password: string) {
+    this.#host = host;
+    this.#username = username;
+    this.#password = password;
+
+    this.setup();
+  }
+
+  setup() {
+    const axiosInstance = axios.create({
+      baseURL: this.#host,
+    });
+    axiosInstance.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+    if (isDev()) {
+      const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+      axiosInstance.defaults.httpsAgent = httpsAgent;
+    }
+    axiosInstance.interceptors.request.use(
+      (config) => {
+        const { url } = config;
+        config.headers['Referer'] = this.#host;
+        config.headers['Host'] = this.#host;
+        if (this.#sid) {
+          config.headers['Cookie'] = `SID=${this.#sid}`;
+        }
+        config.url = `/api/v2/${url}`;
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      },
+    );
+
+    this.#axios = axiosInstance;
+  }
+
+  async login() {
+    const response = await this.#axios.post('auth/login', {
+      username: this.#username,
+      password: this.#password,
+    });
+    if (response.data == 'Failed') {
+      throw new Error('qbittorrent login failed');
+    }
+    const sid = response.headers['set-cookie'][0].split(';')[0].split('=')[1];
+    this.#sid = sid;
+    this.isConnected = true;
+    return response;
+  }
+
+  getVersion() {
+    return this.#axios.get('app/version');
+  }
+
+  // torrent
+
+  addTorrent(data: TorrentOptions) {
+    return this.#axios.post('torrents/add', data);
+  }
+
+  getTorrentList(options: TorrentListOptions) {
+    return this.#axios.get('torrents/info', {
+      params: options,
+    });
+  }
+
+  getTorrentContents(hash: string) {
+    return this.#axios.get('torrents/files', {
+      params: { hash },
+    });
+  }
+
+  renameFile(data: { hash: string; oldPath: string; newPath: string }) {
+    return this.#axios.post('torrents/renameFile', data);
+  }
+}
