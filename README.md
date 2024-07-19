8508af76d6c1d618f3c0873231bd515e57f75ee6
birdgg <birdeggegg@gmail.com>
2024-07-19 21:55:39 +0800

    fix: docker image
    remove nestjs user

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# TODO
-- [ ] move tests to single folder
+# Moe Bangumi
--- apps/server/docker-entrypoint.sh
+++ apps/server/docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -eu
 
-echo "Generating prisma client"
-npx prisma generate --generator client
-
 echo "Run db migrate"
 npx prisma migrate deploy
 
