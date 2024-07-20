b17b46b1a48e4504406ecb00889bbd0b12557aa2
birdgg <birdeggegg@gmail.com>
2024-07-20 22:25:29 +0800

    feat(web): 🍻 replace when first visit

--- apps/web/app/(app)/(home)/page.tsx
+++ apps/web/app/(app)/(home)/page.tsx
@@ -1,7 +1,20 @@
 "use client";
 
 import { BangumiList } from "@/components/bangumi/bangumi-list";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useLocalStorage } from "usehooks-ts";
 
 export default function HomePage() {
+	const router = useRouter();
+	const [isFirstVisit, setValue] = useLocalStorage("first-visit", true);
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		if (isFirstVisit) {
+			setValue(false);
+			router.replace("/setting", { scroll: false });
+		}
+	}, [isFirstVisit]);
+
 	return <BangumiList />;
 }
